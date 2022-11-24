ee680469c817439e46dba12604a5b91fe7fcc11f
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-24 07:22:08 +0100

    Spent my entire day looking for a bastard fetch error(on click to read article it shows 404 error page not found) and still couldn't find it, i am tired honestly

---
+++ components/PostDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const PostDetail = () => {
+  return (
+    <div className="bg-white shadow-lg rounde-lg lg:p-8 pb-12 mb-8">
+      PostDetail
+      </div>
+  )
+}
+
+export default PostDetail
